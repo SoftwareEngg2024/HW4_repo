c4b9c33f5ab1cb7db74c04a0f78958dcef7aec0d
kamatmihir2002 <kamatmihir2002@gmail.com>
2024-10-11 01:08:31 +0000

    Added README, LICENSE and completed task2

---
+++ README.md
@@ -0,0 +1,15 @@
+# Data Wrangling with Shell
+
+
+<p align="center"> 
+    <img src="https://img.shields.io/badge/Language-Shell-blue">
+    <img src="https://img.shields.io/badge/License-MIT-green">
+    <img src="https://img.shields.io/badge/Platform-Linux-blue">
+</p>
+
+This repo contains files for data-wrangling, data analysis using shell code.
+
+
+
+
+
--- task3.sh
+++ task3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gawk -vFPAT='[^,]*|"[^"]*"' '$3 == 2 && $12 ~ /S/ {print $0}' titanic.csv | sed 's/male/M/g; s/feM/F/g' | gawk -vFPAT='[^,]*|"[^"]*"' '{sum += $6; numc += 1} END {print "Average age: "sum/numc}'
+gawk -vFPAT='[^,]*|"[a-zA-Z0-9,.:("") ]*"' '$3 == 2 && $12 ~ /S/ && $6 != "" {print $0}' titanic.csv | sed 's/male/M/g; s/feM/F/g' | gawk -vFPAT='[^,]*|"[a-zA-Z0-9,.:("") ]*"' '{print $0; sum += $6; numc += 1.0} END {print "Average age: "sum/numc}'
